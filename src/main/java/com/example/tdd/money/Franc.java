929802b901cc89783d812e26cbbf95a3c0eae4b7
Jang <sdsd090811@gmail.com>
2024-10-26 14:37:56 +0900

    feat: 6장 끝

--- src/main/java/com/example/tdd/money/Franc.java
+++ src/main/java/com/example/tdd/money/Franc.java
@@ -1,19 +1,11 @@
 package com.example.tdd.money;
 
-public class Franc {
-    private int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Franc dollar = (Franc) obj;
-        return amount == dollar.amount;
     }
 }
---
+++ src/main/java/com/example/tdd/money/Money.java
@@ -0,0 +1,11 @@
+package com.example.tdd.money;
+
+public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount;
+    }
+}
