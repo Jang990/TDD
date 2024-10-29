3cd4d21d5744c2a53fdb26c3d71274d19b943c5d
Jang_Laptop <sdsd090811@gmail.com>
2024-10-29 16:44:21 +0900

    feat: 9장 끝

--- src/main/java/com/example/tdd/money/Money.java
+++ src/main/java/com/example/tdd/money/Money.java
@@ -2,16 +2,21 @@ package com.example.tdd.money;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
 
     public static Dollar dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Franc franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int multiplier);
+
+    public String currency() {
+        return currency;
+    }
 
     @Override
     public boolean equals(Object obj) {
--- src/test/java/com/example/tdd/money/MoneyTest.java
+++ src/test/java/com/example/tdd/money/MoneyTest.java
@@ -29,4 +29,11 @@ class MoneyTest {
         assertEquals(Money.franc(15), five.times(3));
     }
 
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
+
 }
